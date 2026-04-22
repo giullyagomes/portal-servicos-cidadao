9a97790ed6e032f0174a9d96ecc8de58d6aa17d6
giullyagomes <gomes.giullyav@gmail.com>
2026-04-22 20:51:15 -0300

    feat(core): adiciona estrutura inicial da aplicação NestJS

---
+++ apps/backend/src/config/env.config.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  database: {
+    url: process.env.DATABASE_URL,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRATION,
+  },
+});
